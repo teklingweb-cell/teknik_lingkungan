6ea423bc19826459c0df766d04146062f56e343d
teklingweb-cell <tekling.web@gmail.com>
2026-08-31 13:33:56 +0700

    Delete src directory

--- src/app/(site)/penelitian/[slug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import './detail.css';
-
-/** Shares the detail stylesheet between page and not-found. */
-export default function PenelitianDetailLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
